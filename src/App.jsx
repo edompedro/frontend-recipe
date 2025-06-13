2748dcc29f5d1807aba72437b305b7a6905b1be8
edompedro <pedroedomnunes@gmail.com>
2025-06-13 14:22:08 -0300

    :tada: First frontend commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Layout } from "antd";
+import Home from "./pages/Home";
+import FavoritePage from "./pages/Favorite";
+
+function App() {
+  return (
+    <Router>
+        <Layout style={{ minHeight: "100vh", minWidth: "100vw", backgroundColor: "white" }}>
+
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/favorite" element={<FavoritePage />} />
+            </Routes>
+
+        </Layout>
+      </Router>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RecipeProvider } from './context/Recipe/index.jsx'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RecipeProvider>
+      <App />
+    </RecipeProvider>
+  </StrictMode>,
+)
